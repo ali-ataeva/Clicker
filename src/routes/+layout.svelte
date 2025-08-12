560b487146c4f2cf171caecdf86acb0d78353780
Alisa Ataeva <a.606777083e@email.com>
2025-08-12 21:50:01 +0200

    feat(css): Add css styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,44 @@
 
 	let clickCount = $state(0);
 
-	function Add() {
+	function SimpleAdd() {
 		clickCount += 1;
+	}
+	function Reset() {
+		clickCount = 0;
 	}
 </script>
 
 <main>
-	<button onclick={Add}>
-		<p>click here</p>
+	<button class="clickButton" onclick={SimpleAdd}>
+		<p>Click here!</p>
 	</button>
-
 	<p>Your click count {clickCount}</p>
+	<button class="resetButton" onclick={Reset}>
+		<p>Reset clicks</p>
+	</button>
 </main>
+
+<style>
+	.clickButton {
+		background-color: rgb(187, 136, 234);
+		box-shadow: inset rgba(0, 0, 0, 0.435) -2px -2px 2px 2px;
+		width: 200px;
+		height: 200px;
+		border-radius: 100px;
+	}
+	.resetButton {
+		background-color: red;
+		padding: 10px 20px;
+		border-radius: 10px;
+		box-shadow: inset rgba(0, 0, 0, 0.269) -2px -2px 2px 2px;
+	}
+	main {
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 20px;
+	}
+</style>
